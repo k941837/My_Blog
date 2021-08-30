8129273a69e5b389f74ff72fdcaaff7462504852
k941837 <shunta2420@yahoo.co.jp>
2021-08-30 12:28:34 +0900

    ホーム画面修正。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,6 +29,19 @@
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
+
+
+      <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+        </ul>
+      </div>
+
+
+
+      
     
 </div>
 @endsection
